f4456e1d04249102de7e1cc25e37b11358eec711
Mohsin <mohsin2596@gmail.com>
2021-11-08 21:04:03 +0500

    Setup exchange

--- test/helpers.js
+++ test/helpers.js
@@ -2,6 +2,8 @@ import * as utils from 'web3-utils';
 
 export const EVM_REVERT = 'VM Exception while processing transaction: revert';
 
+export const ETHER_ADDRESS = '0x0000000000000000000000000000000000000000';
+
 export const tokens = (n) => {
     return utils.toBN(
         utils.toWei(n.toString(), 'ether')
