03937c9afb7b5c9f0feecee6f5a09e194c6b71f2
Mohsin <mohsin2596@gmail.com>
2021-11-19 17:17:43 +0500

    Charts and transaction history

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './components/App';
+
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
+
 import reportWebVitals from './reportWebVitals';
+
+const store = configureStore();
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+  <Provider store ={store}><App /></Provider>  
   </React.StrictMode>,
   document.getElementById('root')
 );
